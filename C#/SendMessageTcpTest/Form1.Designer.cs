0d62aaa6ee2ac18b4527f45dec83034a6367a99b
FAREAST\pauli <pauli@microsoft.com>
2017-04-17 15:40:45 +0800

    initialization

---
+++ C#/SendMessageTcpTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SendMessageTcpTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 170);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 31);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(222, 95);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ C#/SendMessageTcpTest/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SendMessageTcpTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(this.txtMessage.Text))
+            {
+                SendMessage(this.txtMessage.Text).Wait();
+            }
+        }
+
+        private async Task<IPEndPoint> GetTargetIPEndPoint()
+        {
+            var address = await Dns.GetHostAddressesAsync(Dns.GetHostName());
+            var endpoint = new IPEndPoint(address[0], 8899);
+            return endpoint;
+        }
+
+        private async Task SendMessage(string message)
+        {
+            var remoteServer = "10.86.160.157";
+            var client = new TcpClient(remoteServer,8899);
+            var stream = client.GetStream();
+            var bytes = System.Text.Encoding.ASCII.GetBytes(message);
+            this.txtMessage.Text = "send message:" + message;
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+
+           
+
+            //var bytesToRead = new byte[256];
+            //string response = string.Empty;
+            //int i = 0;
+            //while((i=await stream.ReadAsync(bytesToRead, 0, bytesToRead.Length))!=0)
+            //{
+            //    response += System.Text.Encoding.ASCII.GetString(bytesToRead);
+            //}
+
+            //this.txtMessage.Text += response;
+            stream.Close();
+            client.Close();
+        }
+
+        private void Receive()
+        {
+
+        }
+    }
+}
